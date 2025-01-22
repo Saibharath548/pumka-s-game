3aa23ac34ea798dc606dfd6b1888b42577418ac7
Saibharath548 <kattavsbharath2004@gmail.com>
2025-01-23 00:11:55 +0530

    edho okati

--- Assets/Test/Scripts/spawnerT.cs
+++ Assets/Test/Scripts/spawnerT.cs
@@ -1,9 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class spawnerT : MonoBehaviour
 {
+    [SerializeField] private Sprite[] sp;
+    public SpriteRenderer sR;
+
     [SerializeField] private GameObject[] Platforms;
     [SerializeField] private float PlatspawnTime;
 
@@ -11,9 +12,13 @@ public class spawnerT : MonoBehaviour
     [SerializeField] private float obstacleSpeed;
 
     private Vector3 spawnV;
+    private Transform spawnT;
     // Start is called before the first frame update
     void Start()
     {
+       // sR = GetComponent<SpriteRenderer>();
+
+        spawnT = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -21,6 +26,8 @@ public class spawnerT : MonoBehaviour
     {
         spawnV = new Vector3(Random.Range(-5, 5), 7, 0);
         spawnLoop();
+        sR.sprite = sp[Random.Range(0, sp.Length)];
+        
     }
 
     private void spawnLoop()
@@ -33,11 +40,20 @@ public class spawnerT : MonoBehaviour
             timeuntilSpawn = 0;
         }
     }
+    //GameObject InstantiateRandomScale(GameObject source, float minScale, float maxScale)
+    //{
+    //    GameObject clone = Instantiate(source) as GameObject;
+    //    clone.transform.localScale = Vector3.one * Random.Range(minScale, maxScale);
+    //    return clone;
+    //}
     private void spawn()
     {
+
         GameObject obstacleTospawn = Platforms[Random.Range(0, Platforms.Length)];
 
         GameObject spawnedObstacles = Instantiate(obstacleTospawn, spawnV, Quaternion.identity);
+
+
 
         Rigidbody2D obstacleRB = spawnedObstacles.GetComponent<Rigidbody2D>();
         obstacleRB.velocity = Vector2.down * obstacleSpeed;
