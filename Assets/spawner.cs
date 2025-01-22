722378d941017ae47f25695aa0a918c7aa874836
Saibharath548 <kattavsbharath2004@gmail.com>
2025-01-22 18:46:27 +0530

    commit kotu

---
+++ Assets/spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Platforms;
+    [SerializeField] private float PlatSpawnTimer = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
