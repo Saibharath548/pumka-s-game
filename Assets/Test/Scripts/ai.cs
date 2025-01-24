82e29de02d0ab1f49d8c0e7588d75926cebf4b1d
Saibharath548 <kattavsbharath2004@gmail.com>
2025-01-24 16:05:09 +0530

    yooo

--- Assets/Test/Scripts/ai.cs
+++ Assets/Test/Scripts/ai.cs
@@ -4,6 +4,9 @@ public class ai : MonoBehaviour
 {
     public Transform P;
     public Transform T;
+    public Transform T1;
+
+    [SerializeField]private float CDistance;
     private bool pR = false;
     // Start is called before the first frame update
     void Start()
@@ -30,9 +33,11 @@ public class ai : MonoBehaviour
 
     private void aiMove()
     {
+        
         Vector3 direction = P.position - T.position;
-        T.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(-direction.y,-direction.x) * Mathf.Rad2Deg));
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        T.localRotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(-direction.y,-direction.x) * Mathf.Rad2Deg));
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        T.position = T1.position + Quaternion.Euler(0, 0, angle) * new Vector3(CDistance, 0, 0);
     }
     //private void OnTriggerStay2D(Collider2D collision)
     //{
@@ -51,7 +56,7 @@ public class ai : MonoBehaviour
     {
         if (collision.CompareTag("player"))
         {
-            pR = false;
+            pR = true;
         }
     }
 
